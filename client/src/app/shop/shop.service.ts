a2c32a6dcc22d16beea846419789a065bb21e273
BRian <cimminob@gmail.com>
2020-12-17 15:43:15 -0500

    added routing

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
+import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
+import { map } from 'rxjs/operators';
+import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl='https://localhost:5001/api/'
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(shopParams : ShopParams) {
+    let params = new HttpParams();
+
+    if (shopParams.brandId !== 0) {
+      params = params.append('brandId', shopParams.brandId.toString());
+    }
+
+    if (shopParams.typeId !== 0 ) {
+      params = params.append('typeId', shopParams.typeId.toString());
+    }
+
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageIndex', shopParams.pageSize.toString());
+  
+    //pass in object as second param that observes the HttpResponse rather than just the body
+    //
+    return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
+    //A Pipeable Operator is a function that takes an Observable as its input and returns another Observable.
+    //map HttpResponse to IPagination object
+      .pipe(
+        map(response => {
+          return response.body
+      }));
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
+
+  getBrands() {
+    //returns the body of the HttpResponse
+    return this.http.get<IBrand[]>(this.baseUrl+'products/brands')
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+  }
+}
