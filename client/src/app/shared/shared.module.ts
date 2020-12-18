1e173fb1e9a83ba4cc989492f6e4ae0d549cab55
BRian <cimminob@gmail.com>
2020-12-17 19:56:20 -0500

    end of section 11

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component'; 
+
+
+
+@NgModule({
+  declarations: [PagingHeaderComponent, PagerComponent],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot()  //forRoot enables loading with its providers
+  ],
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent,
+    PagerComponent
+  ]
+})
+export class SharedModule { }
